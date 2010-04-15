e59ec6e36fbce3fac0bbd3c257976980481b1b6a
krisjay <krisjay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-15 13:40:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/grapefile/branches/filestore@229561 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ avi/index.php
@@ -0,0 +1,33 @@
+<? 
+
+/** 
+* Change the path to your folder. 
+* 
+* This must be the full path from the root of your 
+* web space. If you're not sure what it is, ask your host. 
+* 
+* Name this file index.php and place in the directory. 
+*/ 
+
+// Define the full path to your folder from root 
+$path = $_SERVER["DOCUMENT_ROOT"]."/wp-content/plugins/Grapefile/filestore/avi/"; 
+// Open the folder 
+$dir_handle = @opendir($path) or die("Unable to open $path"); 
+
+// Loop through the files 
+while ($file = readdir($dir_handle)) { 
+
+if($file == "." || $file == ".." || $file == "index.php" || $file == "index2.php" ) 
+
+continue; 
+echo filesize($file) . ' bytes - &nbsp;&nbsp; - &nbsp;&nbsp;';
+
+echo "<a href=\"../wp-content/plugins/Grapefile/filestore/avi/$file\">$file</a><br>"; 
+
+} 
+
+// Close 
+closedir($dir_handle); 
+
+?>
+<h3 style="border-top: 1px solid #C0C0C0; margin-bottom: -5px">GrapeFile v0.1</h3>
